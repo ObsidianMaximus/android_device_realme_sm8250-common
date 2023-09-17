48ea256e968f7d3d9b37e4b94dd290eb487ec342
Mashopy <eliasgheeraert@gmail.com>
2023-09-17 20:21:48 +0200

    sm8250-common: Add support for OZO Audio
    Change-Id: I2208a54eae80dc89519c52569996cc948e1c863e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,12 +98,16 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
+TARGET_USES_CUSTOM_C2_MANIFEST := true
+
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
     vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE := \
+    $(COMMON_PATH)/manifest.xml \
+    $(COMMON_PATH)/configs/vintf/c2_manifest_vendor.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_realme_kona
@@ -130,6 +134,7 @@ BOARD_KERNEL_CMDLINE := \
     swiotlb=2048
 
 KERNEL_CUSTOM_LLVM := true
+
 
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_ADDITIONAL_FLAGS += NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
