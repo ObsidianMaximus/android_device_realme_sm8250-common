fc61c7ed684e0e64b8f5fb3778cd18a5670cb83c
Arsalan Ahmad Quraishi <54109777+arsalan-zeus@users.noreply.github.com>
2023-12-03 23:36:38 +0530

    Revert "sm8250-common: Add typeCasting to oplus-fwb"
    This reverts commit c06ea68796b22831399ff48b461909f05df569b0.

--- oplus-fwk/src/com/oplus/util/OplusTypeCastingHelper.java
+++ oplus-fwk/src/com/oplus/util/OplusTypeCastingHelper.java
@@ -1,11 +1,6 @@
 package com.oplus.util;
 
-import android.content.res.OplusBaseConfiguration;
-
 public class OplusTypeCastingHelper {
     
     public OplusTypeCastingHelper() {}
-    public static OplusBaseConfiguration typeCasting(Class<OplusBaseConfiguration> configuration, Object context) {
-        return (OplusBaseConfiguration)context;
-    }
 }
